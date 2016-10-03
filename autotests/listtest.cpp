7209d6ff92a0e43d90ddbd74c45658adf2e3fa46
Elvis Angelaccio <elvis.angelaccio@kde.org>
2016-10-03 19:21:27 +0200

    Update email address
    and also the copyright where I added new code.

--- autotests/listtest.cpp
+++ autotests/listtest.cpp
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2016 Elvis Angelaccio <elvis.angelaccio@kdemail.net>
+ * Copyright (c) 2016 Elvis Angelaccio <elvis.angelaccio@kde.org>
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
--- src/accountmanager.cpp
+++ src/accountmanager.cpp
@@ -1,5 +1,6 @@
 /*
  * Copyright (C) 2014  Daniel Vrátil <dvratil@redhat.com>
+ * Copyright (c) 2016 Elvis Angelaccio <elvis.angelaccio@kde.org>
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
