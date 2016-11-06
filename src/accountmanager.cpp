caeb4ccd40b3cbc9e2c4ee131e7163a3d69b2ff8
Elvis Angelaccio <elvis.angelaccio@kde.org>
2016-11-06 18:41:57 +0100

    Use folder-gdrive as icon
    This is already more adequate than `im-google`, but starting from
    breeze-icon 5.29 it will be the actual Google Drive icon.
    
    CCBUG: 372111

--- src/accountmanager.cpp
+++ src/accountmanager.cpp
@@ -131,7 +131,7 @@ KIO::UDSEntry AccountManager::accountToUDSEntry(const QString &accountNAme)
     entry.insert(KIO::UDSEntry::UDS_FILE_TYPE, S_IFDIR);
     entry.insert(KIO::UDSEntry::UDS_SIZE, 0);
     entry.insert(KIO::UDSEntry::UDS_ACCESS, S_IRUSR | S_IXUSR | S_IRGRP | S_IXGRP | S_IROTH | S_IXOTH);
-    entry.insert(KIO::UDSEntry::UDS_ICON_NAME, QStringLiteral("im-google"));
+    entry.insert(KIO::UDSEntry::UDS_ICON_NAME, QStringLiteral("folder-gdrive"));
 
     return entry;
 }
