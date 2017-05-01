f67eb5756d9c679bdbf08780b85512dc856e3f38
Elvis Angelaccio <elvis.angelaccio@kde.org>
2017-05-01 15:02:58 +0200

    Add plugin for the kaccounts deamon
    This plugin will show a notification whenever a new Google account gets
    added in System Settings. The notification has on Open button that will
    open the gdrive:// URL for the new account.

---
+++ kaccounts/kaccountsplugin.cpp
@@ -0,0 +1,77 @@
+/*
+ * Copyright (c) 2017 Elvis Angelaccio <elvis.angelaccio@kde.org>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ *
+ */
+
+#include "kaccountsplugin.h"
+
+#include <KAccounts/Core>
+
+#include <KLocalizedString>
+#include <KNotification>
+#include <KRun>
+
+#include <QProcess>
+
+GoogleDrivePlugin::GoogleDrivePlugin(QObject *parent)
+    : KAccountsDPlugin(parent)
+{
+}
+
+void GoogleDrivePlugin::onAccountCreated(const Accounts::AccountId accountId, const Accounts::ServiceList &serviceList)
+{
+    Q_UNUSED(serviceList)
+    auto account = Accounts::Account::fromId(KAccounts::accountsManager(), accountId);
+
+    if (account->providerName() != QLatin1String("google")) {
+        return;
+    }
+
+    auto notification = new KNotification(QStringLiteral("new-account-added"));
+    notification->setComponentName(QStringLiteral("gdrive"));
+    notification->setTitle(i18n("%1 account added").arg(account->displayName()));
+    notification->setText(i18n("You can now manage your Google Drive files."));
+    notification->setActions({i18n("Open")});
+
+    QUrl url;
+    url.setScheme(QStringLiteral("gdrive"));
+    url.setPath(QStringLiteral("/%1").arg(account->displayName()));
+
+    connect(notification, static_cast<void (KNotification::*)(unsigned int)>(&KNotification::activated), this, [=]() {
+        KRun::runUrl(url, QStringLiteral("inode/directory"), nullptr);
+    });
+
+    notification->setUrls({url});
+    notification->sendEvent();
+}
+
+void GoogleDrivePlugin::onAccountRemoved(const Accounts::AccountId accountId)
+{
+    Q_UNUSED(accountId)
+}
+
+void GoogleDrivePlugin::onServiceEnabled(const Accounts::AccountId accountId, const Accounts::Service &service)
+{
+    Q_UNUSED(accountId)
+    Q_UNUSED(service)
+}
+
+void GoogleDrivePlugin::onServiceDisabled(const Accounts::AccountId accountId, const Accounts::Service &service)
+{
+    Q_UNUSED(accountId)
+    Q_UNUSED(service)
+}
---
+++ kaccounts/kaccountsplugin.h
@@ -0,0 +1,38 @@
+/*
+ * Copyright (c) 2017 Elvis Angelaccio <elvis.angelaccio@kde.org>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ *
+ */
+
+#pragma once
+
+#include <KAccounts/KAccountsDPlugin>
+
+class GoogleDrivePlugin : public KAccountsDPlugin
+{
+    Q_OBJECT
+    Q_PLUGIN_METADATA(IID "org.kde.kaccounts.DPlugin")
+    Q_INTERFACES(KAccountsDPlugin)
+
+public:
+    GoogleDrivePlugin(QObject *parent = nullptr);
+
+public slots:
+    void onAccountCreated(const Accounts::AccountId accountId, const Accounts::ServiceList &serviceList) override;
+    void onAccountRemoved(const Accounts::AccountId accountId) override;
+    void onServiceEnabled(const Accounts::AccountId accountId, const Accounts::Service &service) override;
+    void onServiceDisabled(const Accounts::AccountId accountId, const Accounts::Service &service) override;
+};
